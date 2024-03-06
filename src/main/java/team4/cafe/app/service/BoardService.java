5f597871664ac37938f2d9139c92492a36e54f62
capliens <qudjchoizn@gmail.com>
2024-03-06 17:51:18 +0900

    관리자 카테고리 관리 v.1

---
+++ src/main/java/team4/cafe/app/service/BoardService.java
@@ -0,0 +1,11 @@
+package team4.cafe.app.service;
+
+import java.util.ArrayList;
+
+import team4.cafe.app.model.vo.CategoryVO;
+
+public interface BoardService {
+
+	ArrayList<CategoryVO> getCategoryList();
+
+}
