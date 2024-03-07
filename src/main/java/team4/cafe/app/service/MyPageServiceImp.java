18e1958daa66b9acfa60ea63372f0f8c144ac07f
imbyeonghun <byeonghun6885@naver.com>
2024-03-07 23:01:50 +0900

    마이페이지 구성 중

--- src/main/java/team4/cafe/app/service/MyPageServiceImp.java
+++ src/main/java/team4/cafe/app/service/MyPageServiceImp.java
@@ -1,5 +1,25 @@
 package team4.cafe.app.service;
 
+import team4.cafe.app.dao.MyPageDAO;
+import team4.cafe.app.model.vo.MemberVO;
+
 public class MyPageServiceImp implements MyPageService {
+
+	private MyPageDAO myPageDao;
+	@Override
+	public int getBoardCount(MemberVO user) {
+		if(user == null) {
+			return 0;
+		}
+		return myPageDao.selectCountBoard(user);
+	}
+
+	@Override
+	public int getCommentCount(MemberVO user) {
+		if(user == null) {
+			return 0;
+		}
+		return myPageDao.selectCountComment(user);
+	}
 
 }
