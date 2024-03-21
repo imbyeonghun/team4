2ee179b04aff3e276d4d8ad09392ecd0ac50c0e4
taewon1122 <tngus9773@naver.com>
2024-03-21 17:59:22 +0900

    Create PostCategoryMainServlet.java

---
+++ src/main/java/team4/cafe/app/controller/manager/postType/PostCategoryMainServlet.java
@@ -0,0 +1,35 @@
+package team4.cafe.app.controller.manager.postType;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+<<<<<<<< HEAD:src/main/java/team4/cafe/app/controller/manager/postType/PostCategoryMainServlet.java
+import team4.cafe.app.model.vo.PostTypeVO;
+import team4.cafe.app.service.PostService;
+import team4.cafe.app.service.PostServiceImp;
+
+@WebServlet("/manager/postCategory")
+public class PostCategoryMainServlet extends HttpServlet {
+========
+@WebServlet("/manager/postType")
+public class PostTypeServlet extends HttpServlet {
+>>>>>>>> main:src/main/java/team4/cafe/app/controller/manager/postType/PostTypeServlet.java
+	private static final long serialVersionUID = 1L;
+    private PostService postService = new PostServiceImp();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+<<<<<<<< HEAD:src/main/java/team4/cafe/app/controller/manager/postType/PostCategoryMainServlet.java
+		ArrayList<PostTypeVO> postTypeList = postService.getPostTypeList();
+		request.setAttribute("postTypeList", postTypeList);
+		request.getRequestDispatcher("/WEB-INF/views/manager/postCategory.jsp").forward(request, response);
+========
+		request.getRequestDispatcher("/WEB-INF/views/manager/postType.jsp").forward(request, response);
+>>>>>>>> main:src/main/java/team4/cafe/app/controller/manager/postType/PostTypeServlet.java
+	}
+
+}
